58dea1041999e1e54cab8e07e930df47006a6fab
Tossig <johannes1993_@hotmail.com>
2025-09-24 14:56:44 +0200

    Nya kata

---
+++ Remove-string-space.js
@@ -0,0 +1,7 @@
+function noSpace(str){
+  return str.split(" ").join("")
+
+  
+  
+  
+}
---
+++ twiceas-old.js
@@ -0,0 +1,4 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  let difference = dadYearsOld - 2 * sonYearsOld; 
+  return Math.abs(difference);
+  }
